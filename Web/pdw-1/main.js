85005b1bef42c40e62863d0986c4ae4b02223a35
Vinicius-S-A <schmaedeckvinicius@gmail.com>
2022-06-07 21:26:53 -0300

    meu fracasso

--- Web/pdw-1/main.js
+++ Web/pdw-1/main.js
@@ -1,3 +1,84 @@
+const storage = localStorage
+
+let names=[]
+let lastnames=[]
+let cep=[]
+let cep2=[]
+let mails=[]
+let quantidadeDeCadastros = 4
+localStorage.setItem('Qnt cadastros', quantidadeDeCadastros)
+
+
+
+
+if (Number(quantidadeDeCadastros) == 0){
+
+localStorage.setItem('names', names)
+localStorage.setItem('lastnames', lastnames)
+localStorage.setItem('cep', cep)
+localStorage.setItem('cep2', cep2)
+localStorage.setItem('mails', mails)
+localStorage.setItem('Qnt cadastros', quantidadeDeCadastros)
+
+
+}else{
+
+  const getnames = localStorage.getItem('names');
+  const getnames2 = JSON.parse(getnames)
+
+  const getlastnames = localStorage.getItem('lastnames');
+  const getlastnames2 = JSON.parse(getlastnames)
+
+  const getcep = localStorage.getItem('cep');
+  const getcep2 = JSON.parse(getcep)
+
+  const getcepDos = localStorage.getItem('cep2');
+  const getcepDos2 = JSON.parse(getcepDos)
+
+  const getmail = localStorage.getItem('mails');
+  const getmail2 = JSON.parse(getmail)
+
+
+  names.push(localStorage.getItem('names'))
+  lastnames.push(localStorage.getItem('lastnames'))
+  cep.push(localStorage.getItem('cep'))
+   cep2.push(localStorage.getItem('cep2'))
+   mails.push(getmail2)
+
+  console.log(names)
+  for(var i =0; i <= quantidadeDeCadastros-1; i++){
+
+
+
+
+      const ul = document.createElement('ul')
+      const h4 = document.createElement('h3')
+      const li1 = document.createElement('li')
+      li1.innerHTML = getnames2[i]
+      const li2 = document.createElement('li')
+      li2.innerHTML = getlastnames2[i]
+      const li3 = document.createElement('li')
+      li3.innerHTML = getcep2[i]
+      const li4 = document.createElement('li')
+      li4.innerHTML = getcepDos2[i]
+      const li5 = document.createElement('li')
+      li5.innerHTML = getmail2[i]
+  
+      h4.innerHTML = i+1
+      ul.appendChild(li1)
+      ul.appendChild(li2)
+      ul.appendChild(li3)
+      ul.appendChild(li4)
+      ul.appendChild(li5)
+  
+      document.getElementById('cadastros').appendChild(h4)
+      document.getElementById('cadastros').appendChild(ul)
+
+
+  }
+  
+}
+
 
 document.getElementById('sendButton').addEventListener('click', function(buttonEvent) {
 
@@ -34,6 +115,48 @@ document.getElementById('sendButton').addEventListener('click', function(buttonE
     return
   }
 
+  names.push(nameForm)
+  lastnames.push(sobrenomeForm)
+  cep.push(cepForm)
+  cep2.push(cep2Form)
+  mails.push(mailForm)
+  localStorage.setItem('names', JSON.parse(names));
+  localStorage.setItem('lastnames', JSON.parse(lastnames));
+  localStorage.setItem('cep', JSON.parse(cep));
+  localStorage.setItem('cep2', JSON.parse(cep2));
+  localStorage.setItem('mails', JSON.parse(mails));
+
+  if(names.length) {
+    const ul = document.createElement('ul')
+    const h4 = document.createElement('h3')
+    const li1 = document.createElement('li')
+    li1.innerHTML = names[quantidadeDeCadastros]
+    const li2 = document.createElement('li')
+    li2.innerHTML = lastnames[quantidadeDeCadastros]
+    const li3 = document.createElement('li')
+    li3.innerHTML = cep[quantidadeDeCadastros]
+    const li4 = document.createElement('li')
+    li4.innerHTML = cep2[quantidadeDeCadastros]
+    const li5 = document.createElement('li')
+    li5.innerHTML = mails[quantidadeDeCadastros]
+
+    h4.innerHTML = quantidadeDeCadastros
+   // ul.appendChild(h4)
+    ul.appendChild(li1)
+    ul.appendChild(li2)
+    ul.appendChild(li3)
+    ul.appendChild(li4)
+    ul.appendChild(li5)
+
+    document.getElementById('cadastros').appendChild(h4)
+    document.getElementById('cadastros').appendChild(ul)
+
+  }
+
+
+
+  quantidadeDeCadastros = quantidadeDeCadastros + 1
+  localStorage.setItem('Qnt cadastros', quantidadeDeCadastros)
 
   
 })
---
+++ Web/pdw-2/main.js
@@ -0,0 +1,78 @@
+let peoples = [];
+const iconEdit = document.createElement('i');
+iconEdit.setAttribute('class', 'fas fa-edit');
+iconEdit.setAttribute('title', 'Editar');
+iconEdit.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;');
+const iconRemove = document.createElement('i');
+iconRemove.setAttribute('class', 'fas fa-trash');
+iconRemove.setAttribute('title', 'Remover');
+iconRemove.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;');
+
+
+const loadPeoples = () => {
+  const itemsJaArmazenados = localStorage.getItem('listaDePessoas');
+  return itemsJaArmazenados ? JSON.parse(itemsJaArmazenados) : [];
+}
+
+const span = () => {
+  const span = document.createElement('span');
+  span.appendChild(iconEdit);
+  span.appendChild(iconRemove);
+
+
+
+  return span;
+} 
+
+const listPeoples = () => {
+  const peoples = loadPeoples();
+  
+  let ul = document.querySelector('ul');
+  if (ul) {
+    ul.remove();
+  }
+  
+  ul = document.createElement('ul');
+ 
+  peoples.forEach((item, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = 
+    
+    `Nome: ${item.name} 
+     Idade: ${item.age}, 
+     Altura: ${item.height}
+     `;
+    
+    li.appendChild(span())
+    ul.appendChild(li);
+  });
+  
+  document.getElementById('list-section').appendChild(ul);
+}
+
+listPeoples();
+
+const addPeople = (event) => {
+  event.preventDefault();
+  const people = {
+    name: document.getElementById('name').value,
+    age: document.getElementById('age').value,
+    height: document.getElementById('height').value,
+  }
+  
+  peoples = loadPeoples();
+
+  peoples.push(people);
+  
+  localStorage.setItem('listaDePessoas', JSON.stringify(peoples));
+
+  document.querySelector('form').reset();
+
+  listPeoples();
+}
+
+
+
+
+const botaoDeAdicionar = document.getElementById('btn');
+botaoDeAdicionar.addEventListener('click', addPeople)
